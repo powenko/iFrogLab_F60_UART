6060f5dcd4e885054922c403f4204a0793c60bbb
Powen Ko <powenko@gmail.com>
2015-09-17 16:09:50 -0700

    add Pin on/off sample for iFrogLab F-60 with Raspberry Pi and Android device.

--- Sample2_PinSwitch/RaspberryPi/SerialDigitalPin.java
+++ Sample2_PinSwitch/RaspberryPi/SerialDigitalPin.java
@@ -2,35 +2,63 @@
 
 import java.util.Date;
 
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.serial.Serial;
 import com.pi4j.io.serial.SerialDataEvent;
 import com.pi4j.io.serial.SerialDataListener;
 import com.pi4j.io.serial.SerialFactory;
 import com.pi4j.io.serial.SerialPortException;
 
+
 /**
  * This example code demonstrates how to perform serial communications using the Raspberry Pi.
- *
+ * this sample is design For iFrogLab F-60 Uart module,
  * @author Robert Savage
+ *         Powen Ko
+ *
  */
-public class SerialExample {
+public class SerialDigitalPin  {
     
     public static void main(String args[]) throws InterruptedException {
-
-        System.out.println("<--Pi4J--> Serial Communication Example ... started.");
-        System.out.println(" ... connect using settings: 9600, N, 8, 1.");
-        System.out.println(" ... data received on serial port should be displayed below.");
-        
         // create an instance of the serial communications class
         final Serial serial = SerialFactory.createInstance();
+        final GpioController  gpio=GpioFactory.getInstance();
+        
+        final GpioPinDigitalOutput led2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
+        final GpioPinDigitalOutput led3 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03);
+        final GpioPinDigitalOutput led4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04);
+        
+        led4.high();
+        led2.high();
+        led3.high();
         
         // create and register the serial data listener
         serial.addListener(new SerialDataListener() {
             @Override
             public void dataReceived(SerialDataEvent event) {
                 // print out the data received to the console
+                // print out the data received to the console
+                String t1=event.getData();
+                System.out.print(t1);
                 
-                System.out.print(event.getData());
+                if(t1.startsWith("m4 on")){
+                    led4.high();
+                }else if(t1.startsWith("m4 off")){
+                    led4.low();
+                }else if(t1.startsWith("m2 on")){
+                    led2.high();
+                }else if(t1.startsWith("m2 off")){
+                    led2.low();
+                }else if(t1.startsWith("m3 on")){
+                    led3.high();
+                }else if(t1.startsWith("m3 off")){
+                    led3.low();
+                }
             }
         });
         
@@ -44,21 +72,17 @@ public class SerialExample {
                     // write a formatted string to the serial transmit buffer
                     // 把現在的時間，傳遞過去。
                     serial.write("CURRENT TIME: %s", new Date().toString());
+                    // write a individual characters to the serial transmit buffer
                     // 把特定的字元 ，傳遞過去。
                     serial.write('\r');
                     serial.write('\n');
-                    
-
-                    
-                    // 把字串傳遞過去，並且做跳行的動作（ CR+LF ）
-                    // serial.writeln("Third Line");
                 }
                 catch(IllegalStateException ex){
                     ex.printStackTrace();
                 }
                 
                 // wait 1 second before continuing
-                Thread.sleep(1000);
+                Thread.sleep(5000);
             }
             
         }
